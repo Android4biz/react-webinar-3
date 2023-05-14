4836b3d067389530578b47a19f6fc486a6e6a215
Andrei <andreikravtsov88@gmail.com>
2023-05-14 20:39:06 +0300

    style

--- src/app.js
+++ src/app.js
@@ -9,19 +9,21 @@ import { counter } from "./counter.js";
  */
 function App({ store }) {
     const list = store.getState().list;
-    
-    return <div className="App">
+
+    return (
+        <div className="App">
             <div className="App-head">
                 <h1>Приложение на чистом JS</h1>
             </div>
             <div className="App-controls">
                 <button
                     onClick={() => {
-                        const code = counter()
+                        const code = counter();
                         store.addItem({
                             code,
-                            title: `Новая запись`,
-                        });}}
+                            title: `Новая запись`
+                        });
+                    }}
                 >
                     Добавить
                 </button>
@@ -37,20 +39,23 @@ function App({ store }) {
                                 }
                                 onClick={() => store.selectItem(item.code)}
                             >
-                                <div className="Item-code">
-                                    {item.code}
-                                </div>
-                                <div className="Item-title">
-                                    {item.title}
-                                </div>
-                                <div className="Item-title">
-                                    {item.count > 0 &&
-                                        <div>
-                                            | Выделяли {item.count} раз
-                                        </div>}
+                                <div className="left-block">
+                                    <div className="Item-code">
+                                        {item.code}
+                                    </div>
+                                    <div className="Item-title">
+                                        {item.title}
+                                    </div>
+                                    <div className="Item-title">
+                                        {item.count > 0 &&
+                                            <div className="alocate">
+                                                | Выделяли {item.count} раз
+                                            </div>}
+                                    </div>
                                 </div>
                                 <div className="Item-actions">
                                     <button
+                                        className="btn"
                                         onClick={() =>
                                             store.deleteItem(item.code)}
                                     >
@@ -62,7 +67,8 @@ function App({ store }) {
                     )}
                 </div>
             </div>
-        </div>;
+        </div>
+    );
 }
 
 export default App;
